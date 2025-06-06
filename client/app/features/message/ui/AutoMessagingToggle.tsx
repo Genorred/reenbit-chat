9ce420e5187d87f578d7196d559f11758f9c0bc2
Genorred <gmihasik@gmail.com>
2025-06-06 11:57:47 +0200

    automessagint revalidation

--- client/app/features/message/ui/AutoMessagingToggle.tsx
+++ client/app/features/message/ui/AutoMessagingToggle.tsx
@@ -7,33 +7,29 @@ import type {ServerMessage} from "~/features/message/model/ServerMessage";
 import {queryClient} from "~/shared/lib/queryClient";
 import type {MessageI} from "~/features/message/model/Message";
 import {getChatQueryKey} from "~/features/message/lib/getChatQueryKey";
-import {useParams} from "react-router";
 
 const AutoMessagingToggle = () => {
-        const {chatId = ''} = useParams();
         const {isEnabled, setAutoMessage} = useAutoMessageStore();
         const addToast = useToastStore((state) => state.addToast);
         const url = new URL(import.meta.env.VITE_WS_API_URL + '/auto-chat' as string);
         useWebSocket<ServerMessage>(url.href, {
                 shouldReconnect: event => event.type === "CONNECT",
                 onMessage: (event) => {
-                    const data: MessageI & {
+                    const data: {
                         firstName: string,
                         lastName: string,
+                        _doc: MessageI
                     } = JSON.parse(event.data);
                     addToast({
                         type: "info",
                         message: `You got a message from ${data.firstName} ${data.lastName}`,
                     })
 
-                    const queryKey = getChatQueryKey(chatId);
+                    const queryKey = getChatQueryKey(data._doc.chatId);
 
+                    console.log(data)
                     queryClient.setQueryData(queryKey, (oldData: MessageI[] = []) => {
-                        return oldData.map(msg =>
-                            msg._id === data._id
-                                ? {...msg, content: data.content}
-                                : msg
-                        )
+                        return [...oldData, data._doc];
                     })
                 }
             },
